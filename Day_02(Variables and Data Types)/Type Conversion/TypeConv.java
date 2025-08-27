fc0eaf037320f4cdf30de2c409203dc8354a71a7
Hemrajjj13 <iamhemraj13@gmail.com>
2025-08-27 23:00:39 +0530

    Type Conversion

--- Day_02(Variables and Data Types)/Type Conversion/TypeConv.java
+++ Day_02(Variables and Data Types)/Type Conversion/TypeConv.java
@@ -2,7 +2,12 @@ import java.util.*;
 
 public class TypeConv {
     public static void main(String args[]) {
-       
+       int a = 25;
+       long b = a;
+       System.out.println(b);
+       Scanner sc = new Scanner(System.in);
+       float number = sc.nextInt();
+       System.out.println(number);
     }
 }
 
