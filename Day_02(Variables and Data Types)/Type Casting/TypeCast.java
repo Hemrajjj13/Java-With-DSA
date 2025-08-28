301d6197bb50a602bbd3743de7af1d4c430e3a73
Hemrajjj13 <iamhemraj13@gmail.com>
2025-08-28 11:43:10 +0530

    COmpleting the day 2 stuff

---
+++ Day_02(Variables and Data Types)/Type Casting/TypeCast.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class TypeCast {
+    public static void main(String args[]) {
+       float a = 25.13f;
+       int b = (int) a;
+       System.out.println(b);
+
+       char ch = 'a';
+       char ch2 = 'c';
+       int number = ch;
+       int number2 = ch2;
+       System.out.println(number);
+       System.out.println(number2);
+    }
+}
+
