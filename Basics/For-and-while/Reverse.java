362490d6e7105436e91f7d4130f801d43a2e5fd1
Hemrajjj13 <iamhemraj13@gmail.com>
2025-10-08 09:54:57 +0530

    For and while Loops

---
+++ Basics/For-and-while/Reverse.java
@@ -0,0 +1,18 @@
+
+
+public class Reverse {
+     public static void main(String[] args) {
+        int num = 485485;
+
+        int ans = 0;
+
+        while (num > 0) {
+            int rem = num % 10;
+            num /= 10;
+
+            ans = ans * 10 + rem;
+        }
+
+        System.out.println(ans);
+     }
+}
---
+++ Basics/For-and-while/While.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class While {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the nth number:");
+        int n = scan.nextInt();
+        
+
+        int a = 0;
+        int b = 1;
+        int count = 2;
+
+        while (count <= n) {
+            int temp = b;
+            b = b + a;
+            a = temp;
+            count++;
+        }
+        System.out.println(b);
+    }
+}
