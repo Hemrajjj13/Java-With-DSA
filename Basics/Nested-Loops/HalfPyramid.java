bd0c76a49d49625794a6a707d6555916609021a3
Hemrajjj13 <iamhemraj13@gmail.com>
2025-10-11 21:33:23 +0530

    Printed Some patterns using LOops

---
+++ Basics/Nested-Loops/HalfPyramid.java
@@ -0,0 +1,16 @@
+
+public class HalfPyramid {
+    public static void main(String[] args) {
+        int n = 8;
+
+        for (int line=1; line<=n; line++){
+            //NUmbers print 
+            {
+                for (int number=1; number<=line; number++){
+                    System.out.print(number);
+                }
+                System.out.println();
+            }
+        }
+    }
+}
