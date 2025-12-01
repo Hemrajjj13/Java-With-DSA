7c5653e7c10791ac900d5c9a59da7a1992b00a35
Hemrajjj13 <iamhemraj13@gmail.com>
2025-12-01 22:51:49 +0530

    Add program to read and print an integer

---
+++ Strivers-DSA-Sheet/Basics/One.java
@@ -0,0 +1,10 @@
+// Complete the function printNumber which takes an integer input from the user and prints it on the screen.
+import java.util.*;
+
+public class One {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.print(num);
+    }
+}
