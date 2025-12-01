171e2156791aa961eb7401c76c1cf17e6e918167
Hemrajjj13 <iamhemraj13@gmail.com>
2025-12-01 22:20:46 +0530

    Add Java program that iterates from 1 to 904 and prints how many numbers are divisible by 3

---
+++ Arrays/Basics/New.java
@@ -0,0 +1,13 @@
+public class New {
+    public static void main(String[] args) {
+        
+
+        int count = 0;
+        for (int i = 1; i <= 904; i++) {
+            if (i % 3 == 0) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
