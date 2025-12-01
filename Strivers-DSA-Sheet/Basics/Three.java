635319dae633853263612b527442e615ac88f9c5
Hemrajjj13 <iamhemraj13@gmail.com>
2025-12-01 23:07:14 +0530

    Add Java program that prints the weekday based on integer input, handling invalid days

---
+++ Strivers-DSA-Sheet/Basics/Three.java
@@ -0,0 +1,39 @@
+import java.util.*;
+/*
+Given the integer day denoting the day number, print on the screen which day of the week it is. Week starts from Monday and for values greater than 7 or less than 1, print Invalid.
+
+Ensure only the 1st letter of the answer is capitalised.
+*/
+public class Three {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter day:");
+        int day = sc.nextInt();
+
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturady");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid check");
+        }
+    }
+}
