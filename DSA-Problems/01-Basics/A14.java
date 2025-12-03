09092b95da170d4318921918c75215f0032b5627
Hemrajjj13 <iamhemraj13@gmail.com>
2025-12-03 11:11:40 +0530

    implement currency converter from INR to USD using fixed rate

---
+++ DSA-Problems/01-Basics/A14.java
@@ -0,0 +1,15 @@
+import java.util.*;
+/*
+Input currency in rupees and output in USD.
+*/
+public class A14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a  Rupees: ");
+        double num = sc.nextDouble();
+
+        double dollars = num * 0.011;
+        System.out.println(dollars);
+        
+    }
+}
