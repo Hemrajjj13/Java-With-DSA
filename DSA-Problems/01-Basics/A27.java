a57162a5de96eeccd7c4a85890fcf9d6559ee0ae
Hemrajjj13 <iamhemraj13@gmail.com>
2025-12-04 12:03:36 +0530

    Add program to calculate perimeter of a parallelogram

---
+++ DSA-Problems/01-Basics/A27.java
@@ -0,0 +1,13 @@
+import java.util.*;
+/*
+Perimeter Of Parallelogram
+*/
+public class A27 {
+    public static void main(String[] args) {
+        int sidea = 15;
+        int sideb = 30;
+        int pM = 2 * (sidea + sideb);
+
+        System.out.println(pM);
+    }
+}
