68edc0ae0f0c76a29456c8f8c3fbfa7125f4428a
Hemrajjj13 <iamhemraj13@gmail.com>
2025-12-04 12:01:08 +0530

    Add program to calculate perimeter of an equilateral triangle

---
+++ DSA-Problems/01-Basics/A26.java
@@ -0,0 +1,12 @@
+import java.util.*;
+/*
+Perimeter Of Equilateral Triangle
+*/
+public class A26 {
+    public static void main(String[] args) {
+        int side = 15;
+        int pM = 3 * side;
+
+        System.out.println(pM);
+    }
+}
