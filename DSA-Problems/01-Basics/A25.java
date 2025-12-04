17333870874a99bb0b58bfed7f54c7da8897e57e
Hemrajjj13 <iamhemraj13@gmail.com>
2025-12-04 11:30:33 +0530

    add circle perimeter calculation program

---
+++ DSA-Problems/01-Basics/A25.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+/*
+Perimeter Of Circle
+*/
+public class A25 {
+    public static void main(String[] args) {
+        int r = 15;
+        double pi = 3.14;
+        double pM = 2 * pi * 4;
+
+        System.out.println(pM);
+    }
+}
