d7b51f9072b1870fe8b411b667889bbe635a1720
Hemrajjj13 <iamhemraj13@gmail.com>
2025-12-12 14:37:53 +0530

    feat(patterns): add pattern18 generating rows of decreasing characters starting from A+(n-1)

--- Basics/Patterns/Paaterns.java
+++ Basics/Patterns/Paaterns.java
@@ -62,9 +62,10 @@ public class Paaterns {
 
     public static void pattern18(int n) {
         for (int i = 0; i < n; i++) {
-            char ch = (char)('A' + i);
+            char ch = (char)('A' + n - 1);
             for (int j = 0; j <= i; j++) {
-                System.out.print(ch + " ");
+                System.out.print(ch);
+                ch--;
             }
             System.out.println();
         }
