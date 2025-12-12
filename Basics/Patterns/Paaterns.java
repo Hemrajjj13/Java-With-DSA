02d700cae35cd5666dbd5ee72946577005eee658
Hemrajjj13 <iamhemraj13@gmail.com>
2025-12-12 14:33:07 +0530

    Add alphabet pattern generation methods (patterns 14–17) and integrate pattern17 into main

--- Basics/Patterns/Paaterns.java
+++ Basics/Patterns/Paaterns.java
@@ -5,7 +5,7 @@ public class Paaterns {
         System.out.print("Enter a Number: ");
         int n = sc.nextInt();
         
-        pattern14(n);
+        pattern18(n);
     }
 
     public static void pattern14(int n) {
@@ -15,9 +15,61 @@ public class Paaterns {
                 System.out.print(ch + " ");
                 
             }
-
- 
             System.out.println();
         }
     } 
+
+    public static void pattern15(int n) {
+        for (int i = 0; i < n; i++) {
+            
+            for (char ch = 'A'; ch <= 'A' + (n - i - 1); ch++) {
+                System.out.print(ch + " ");
+                
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern16(int n) {
+        for (int i = 0; i < n; i++) {
+            char ch = (char)('A' + i);
+            for (int j = 0; j <= i; j++) {
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern17(int n) {
+        for (int i = 0; i < n; i++) {
+                                    // 4
+            for (int j = 0; j < (n - i - 1); j++) {
+                System.out.print(" ");
+            }
+            char ch = 'A';
+            int breakPoint = (2*i+1) / 2;
+            for (int j = 1; j <= (2 * i + 1); j++) {
+                System.out.print(ch);
+                if (j <= breakPoint) ch++; 
+                else ch--; 
+            }
+            for (int j = 0; j < (n - i - 1); j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void pattern18(int n) {
+        for (int i = 0; i < n; i++) {
+            char ch = (char)('A' + i);
+            for (int j = 0; j <= i; j++) {
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    
 }
