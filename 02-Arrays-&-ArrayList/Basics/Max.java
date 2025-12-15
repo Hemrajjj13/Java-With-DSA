3ffff6e8664c9743396140d7f697a2a2f2519577
Hemrajjj13 <iamhemraj13@gmail.com>
2025-12-15 20:00:48 +0530

    added a function into Max.java to find max number between range

--- 02-Arrays-&-ArrayList/Basics/Max.java
+++ 02-Arrays-&-ArrayList/Basics/Max.java
@@ -4,11 +4,21 @@ public class Max {
     public static void main(String[] args) {
         int[] arr = {1, 2, 33, 4, 5, 6, 7, 8, 9, 10, 45, 31};
         System.out.println(MaxItem(arr));
+        System.out.println(MaxItemInRange(arr, 2, 8));
     }
 
     static int MaxItem(int[] arr) {
         int max = arr[0];
         for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+    static int MaxItemInRange(int[] arr, int start, int end) {
+        int max = arr[start];
+        for (int i = start; i <= end; i++) {
             if (arr[i] > max) {
                 max = arr[i];
             }
